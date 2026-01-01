5e060d933b391f5a697c9937958860c5fd113b17
Hunadi Ntjana <hunadin@mtdevelopment.co.za>
2026-01-01 12:02:22 +0200

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,19 +3,42 @@ import cors from "cors";
 import dotenv from "dotenv";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-
+const __dirname = path.resolve();
 
 //middleware
 app.use(express.json());
-app.use(cors({origin:"http://localhost:5173"}));
+app.use(
+  cors({
+    origin:
+      process.env.NODE_ENV === "production"
+        ? true // allow Render domain
+        : "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/notes", notesRoutes);
 
+app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+// ---------------- FRONTEND (PRODUCTION) ----------------
+if (process.env.NODE_ENV === "production") {
+  const frontendPath = path.resolve(__dirname, "frontend/dist");
+
+  // Serve static files
+  app.use(express.static(frontendPath));
+
+  // SPA catch-all
+  app.use((req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+  });
+}
 
 connectDB().then(()=>{
 
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL : "http://localhost:5001/api",
+    baseURL: import.meta.env.MODE ==="development" ? "http://localhost:5001/api" : "/api",
+    withCredentials: true
 })
 
 export default api;
